9a0ff2e76196088e4a7ea415180499794d6fc2cf
Schumynet <netprime311@gmail.com>
2025-09-22 01:19:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function watch() {
   const id = document.getElementById("videoId").value;
   const video = document.getElementById("video");
   const hls = new Hls();
-  hls.loadSource(`https://fabio-backend.onrender.com/watch?id=${id}`);
+  hls.loadSource(`https://vixstreamproxy.onrender.com/watch?id=${id}`);
   hls.attachMedia(video);
 }
